62d668f77a543cb6104a15d9df05a2db9f9a7b4b
Lazarus Gates <22086359+lazarusgates@users.noreply.github.com>
2025-04-10 11:41:02 +1000

    implement turn order based on speed
    change enemies array to a dict and create function calc_order which creates an array with enemy ids and player sorted descending by speed

--- test_combined/battle_manager.gd
+++ test_combined/battle_manager.gd
@@ -1,12 +1,12 @@
 extends Node2D
-
 var enemy_drone_scene = preload("res://enemies/enemy_drone.tscn")
 
 var num_enemies
-var enemies = []
+var enemies = {}
+var order = []
 var turns
 var selecting = false
-var selected_enemy = 0
+var selected_enemy = 1
 
 var rewarded_xp = 0
 var rewarded_money = 0
@@ -15,14 +15,16 @@ signal spawn_keys
 signal return_to_menu
 
 func _ready():
-	selected_enemy = 0
+	selected_enemy = 1
 	num_enemies = randi_range(1, 5)
 	for i in range(1, num_enemies+1):
-		enemies.append(enemy_drone_scene.instantiate())
-		enemies[i-1].set_enemy("rock", 1)
-		self.add_child(enemies[i-1])
-		enemies[i-1].global_position.y = 200
-		enemies[i-1].global_position.x = 200 * i
+		enemies[i] = enemy_drone_scene.instantiate()
+		enemies[i].set_enemy("rock", 1, i)
+		self.add_child(enemies[i])
+		enemies[i].global_position.y = 200
+		enemies[i].global_position.x = 200 * i
+	print("num enemies: ", len(enemies))
+	calc_order()
 
 func _input(event):
 	if selecting:
@@ -36,6 +38,46 @@ func _input(event):
 			player_attack(selected_enemy)
 			selecting = false
 			enemies[selected_enemy].selected(false)
+
+func calc_order():
+	#calculate the order of the enemies
+	#sorts order in DESCENDING order i.e. fastest is first
+	var idx = 1
+	for i in range(1, len(enemies)+1):
+		while idx <= len(enemies):
+			print("this iteration's speed: ", enemies[i].return_speed())
+			if order.is_empty():
+				print("first entry is ", enemies[i].return_speed())
+				order.append(idx)
+				print("first entry order: ", order)
+				break
+			elif idx == len(order):
+				print("current slowest: ", enemies[i].return_speed())
+				order.append(i)
+				break
+			elif enemies[i].return_speed() >= enemies[order[idx-1]].return_speed():
+				order.insert(idx-1, i)
+				print(i, "th iteration order: ", order)
+				break
+			else:
+				idx += 1
+	
+	#insert the player's position in the turn order
+	if PlayerStatsManager.player_speed >= enemies[order[0]].return_speed():
+		order.insert(0, -1)
+		print("player is fastest: ", order)
+	elif PlayerStatsManager.player_speed <= enemies[order[len(order)-1]].return_speed():
+		order.append(-1)
+		print("player is slowest: ", order)
+	else:
+		idx = 1
+		while idx <= len(order):
+			if PlayerStatsManager.player_speed >= enemies[order[idx-1]].return_speed():
+				order.insert(idx-1, -1)
+				break
+			else:
+				idx += 1
+	print(order)
 
 func selecting_enemy():
 	selecting = true
@@ -51,33 +93,36 @@ func change_selection(direction):
 	elif len(enemies) != 1:
 		if direction == "left":
 			enemies[selected_enemy].selected(false)
-			if selected_enemy == 0:
-				selected_enemy = num_enemies - 1
+			if selected_enemy == 1:
+				selected_enemy = num_enemies
 			else:
 				selected_enemy -= 1
 			enemies[selected_enemy].selected(true)
 				
 		elif direction == "right":
 			enemies[selected_enemy].selected(false)
-			if selected_enemy == num_enemies - 1:
-				selected_enemy = 0
+			if selected_enemy == num_enemies:
+				selected_enemy = 1
 			else:
 				selected_enemy += 1
 			enemies[selected_enemy].selected(true)
 
 func kill_enemy():
-		rewarded_xp += enemies[selected_enemy].return_xp()
-		rewarded_money += enemies[selected_enemy].return_money()
-		print("total xp: ", rewarded_xp)
-		print("total money: ", rewarded_money)
-		enemies[selected_enemy].die()
-		for i in range(selected_enemy, len(enemies)-1):
-			enemies[i] = enemies[i+1]
-		enemies = enemies.slice(0, len(enemies)-1)
-		num_enemies -= 1
-		selected_enemy = 0
-		if enemies.is_empty():
-			battle_end()
+	#calculate experience and money dropped by dying enemy
+	rewarded_xp += enemies[selected_enemy].return_xp()
+	rewarded_money += enemies[selected_enemy].return_money()
+	print("total xp: ", rewarded_xp)
+	print("total money: ", rewarded_money)
+	
+	#kill the enemy instance
+	enemies[selected_enemy].die()
+	
+	#remove dead enemy from enemies dict
+	enemies.erase(selected_enemy)
+	num_enemies -= 1
+	selected_enemy = 1
+	if enemies.is_empty():
+		battle_end()
 
 func battle_end():
 	PlayerStatsManager.add_xp(rewarded_xp)
--- test_combined/enemies/enemy_drone.gd
+++ test_combined/enemies/enemy_drone.gd
@@ -5,6 +5,8 @@ var level
 var strong_against
 var weak_to
 
+var id: int
+
 #stats
 var attack: int = 40
 var health: int = 75
@@ -14,9 +16,9 @@ var speed: int = 30
 var money = 50
 var xp = 100
 
-
-func set_enemy(type: String, enemy_level: int):
+func set_enemy(type: String, enemy_level: int, unique_id: int):
 	genre_type = type
+	unique_id = id
 	match genre_type:
 		"pop":
 			strong_against = "punk"
@@ -45,6 +47,7 @@ func set_enemy(type: String, enemy_level: int):
 	$enemy_health.text = str(health)
 	
 	attack = attack * level
+	speed = (speed * level) + randi_range(-2*level, 2*level)
 
 func selected(toggle: bool):
 	if toggle:
@@ -69,6 +72,9 @@ func deal_damage(turn, player_health):
 
 func return_speed():
 	return(speed * level)
+
+func return_id():
+	return(id)
 
 func return_money():
 	var variation = randi_range(-10*level/2, 10*level/2)
